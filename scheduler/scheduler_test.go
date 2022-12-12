a4ce4b793ad70007fe149d5c268a1c17f1e3a689
yuandongx <yuandongx@126.com>
2022-12-12 18:09:33 +0800

    1231

--- scheduler/scheduler_test.go
+++ scheduler/scheduler_test.go
@@ -7,5 +7,7 @@ import (
 
 func TestStart(t *testing.T) {
 	tm := time.Time{}
+	now := time.Now()
 	t.Log(tm.IsZero())
+	t.Log(tm.After(now))
 }
--- scheduler/trigger.go
+++ scheduler/trigger.go
@@ -14,165 +14,60 @@ const (
 	Saturday
 )
 
-// 任务触发器接口
-type TriggerInterface interface {
-	NextRuntime() time.Time
-	InRange() bool
-	UpdateLastTime(t *time.Time)
+type WorkTime struct {
+	start time.Time
+	stop  time.Time
 }
 
-// 时间范围
-type TimeRange struct {
-	start time.Time // 开始时间
-	stop  time.Time // 结束时间
-}
-
-// 在时间范围内
-func (t TimeRange) InRange() bool {
-	now := time.Now()
-	// 开始时间为 0 或者 开始时间之后 当前时间可能有效
-	f1 := t.start.IsZero() || now.After(now) || now.Equal(t.start)
-	// 结束时间为 0 或者 结束时间之前 当前时间可能有效
-	f2 := t.stop.IsZero() || now.Before(now) || now.Equal(t.stop)
-	// 开始 和结束时间同时有效 该时间范围才有效
-	return f1 && f2
-}
-
-type Interval struct {
-	TimeRange
+type Trigger struct {
+	StartTime   time.Time
+	StopTime    time.Time
+	Deviation   time.Duration //时间偏差
 	LastRunTime time.Time
-	Interval    time.Duration // 时间间隔
-	Deviation   time.Duration // 偏差容忍度
+	NextRuntime time.Time
+	Interval    time.Duration
+	SkippedDays []int // 那些天跳过
+	JustWorkDay bool
 }
 
-// i: 时间间隔
-// d： 偏差容忍
-// t: 有效期
-func NewInterval(i time.Duration, d time.Duration, t TimeRange) *Interval {
-	return &Interval{
-		TimeRange:   t,
-		LastRunTime: TIMEZERO,
-		Interval:    i,
-		Deviation:   d,
-	}
-}
-
-func (i Interval) InRange() bool {
-	return i.TimeRange.InRange()
-}
-
-func (d *Interval) UpdateLastTime(t time.Time) {
-	d.LastRunTime = t
-}
-func (i *Interval) NextRuntime() time.Time {
-	if i.InRange() {
-		if i.LastRunTime.IsZero() {
-			return time.Now()
-		} else {
-			return i.LastRunTime.Add(i.Interval)
-		}
-	}
-	return TIMEZERO
-}
-
-type DateTime struct {
-	RunAt       time.Time
-	Deviation   time.Duration
-	LastRunTime time.Time
-}
-
-func NewDateTime(runat time.Time, d time.Duration) *DateTime {
-	return &DateTime{
-		RunAt:       runat,
-		Deviation:   d,
-		LastRunTime: TIMEZERO,
-	}
-}
-
-func (d DateTime) InRange() bool {
-	now := time.Now()
-	return d.RunAt.Sub(now) < d.Deviation
-}
-
-func (d *DateTime) UpdateLastTime(t time.Time) {
-	d.LastRunTime = t
-}
-
-func (d DateTime) NextRuntime() time.Time {
-	if !d.InRange() {
+// 获取下次执行时间
+func (c *Trigger) GetNextRunTime() time.Time {
+	if c.Expired() {
 		return TIMEZERO
 	}
-	return d.RunAt
-}
-
-type Corn struct {
-	TimeRange
-	Years       []int
-	Months      []int
-	Days        []int
-	Hours       []int
-	Minutes     []int
-	Seconds     []int
-	Weekdays    []int
-	Deviation   time.Duration
-	LastRunTime time.Time
-}
-
-func (c Corn) InRange() bool {
-	return c.TimeRange.InRange()
-}
-
-func (c *Corn) UpdateLastTime(t time.Time) {
-	c.LastRunTime = t
-}
-
-func (c *Corn) NextRuntime() time.Time {
-	now := time.Now()
-	year := now.Year()
-	month, _ := ToInt(now.Month())
-	day := now.Day()
-	weekday, _ := ToInt(now.Weekday())
-	hour := now.Hour()
-	minute := now.Minute()
-	second := now.Second()
-
-	y := len(c.Years) == 0 || IntContain(c.Years, year)
-	m := len(c.Months) == 0 || IntContain(c.Months, month)
-	d := len(c.Days) == 0 || IntContain(c.Days, day)
-	w := len(c.Weekdays) == 0 || IntContain(c.Weekdays, weekday)
-	h := len(c.Hours) == 0 || IntContain(c.Hours, hour)
-	// n := len(c.Minutes) == 0 || IntContain(c.Minutes, minute)
-
-	if y && m && d && w && h {
-		startMinut := 0
-		if len(c.Minutes) > 0 {
-			for _, m := range c.Minutes {
-				if minute <= m {
-					startMinut = m
-					break
-				}
-			}
-			if startMinut == 0 {
-				startMinut = c.Minutes[0]
-			}
-		}
-		startSecond := 0
-		if len(c.Seconds) > 0 {
-
-			for _, s := range c.Seconds {
-				if second < s {
-					startSecond = s
-					break
-				}
-			}
-			if startSecond == 0 {
-				startSecond = c.Seconds[0]
-			}
-
-		}
-		return time.Date(year, now.Month(), day, hour, startMinut, startSecond, 0, TimeLoaction())
+	if c.Pending() {
+		return c.StartTime
 	}
+	now := time.Now()
+
+	// 跳过那些天
+	s := len(c.SkippedDays) == 0 || !IntContain(c.SkippedDays, now.Day())
+
 	return TIMEZERO
+}
+
+// 任务是否在有效期内
+func (t *Trigger) Ongoing() bool {
+	now := time.Now()
+	// 0 < now 所以当前时间必须在start time 之后
+	// 如果结束时间不为 0 则结束时间应在当前时间之后
+	return t.StartTime.Before(now) && (t.StopTime.IsZero() || t.StopTime.After(now))
+}
+
+// 任务等待中
+func (t *Trigger) Pending() bool {
+	now := time.Now()
+	return t.StartTime.IsZero() && now.Before(t.StartTime)
+}
+
+// 过期的
+func (t *Trigger) Expired() bool {
+	now := time.Now()
+	return t.StopTime.IsZero() && now.After(t.StopTime)
+}
+
+func NewTrigger() *Trigger {
+	return &Trigger{}
 }
 
 func ToInt(w Any) (int, bool) {
