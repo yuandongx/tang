993a69fc6a8cd4d98f27ed87cc287897c2ce03b4
孙明夺 <sunmingduo@datagrand.com>
2023-05-06 14:41:17 +0800

    U

---
+++ sqldb/db_data.go
@@ -0,0 +1,7 @@
+package sqldb
+
+type DV struct {
+	Name  string
+	Value any
+	Type  int
+}
---
+++ sqldb/session_test.go
@@ -0,0 +1,11 @@
+package sqldb
+
+import (
+	"testing"
+)
+
+func TestSession_Query(t *testing.T) {
+
+	s := Session{}
+	s.Query("")
+}
