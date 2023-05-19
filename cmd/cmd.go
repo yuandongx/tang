1735652e931f0a9c739686083cd77fe9beeb4099
孙明夺 <sunmingduo@datagrand.com>
2023-05-19 15:21:11 +0800

    U

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,17 +1,25 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
 
 type Command = cobra.Command
 
 var (
 	rootCmd = Command{
-		Use:   "stock-cli",
+		Use:   "stock",
 		Short: "An app for stock do some funny things.",
 		Long:  "An app for stock that sync some stock info and note.",
 	}
 )
 
-func init() {
-
+func RunCmd() {
+	err := rootCmd.Execute()
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
--- cmd/init-db.go
+++ cmd/init-db.go
@@ -1,1 +1,22 @@
 package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	var force bool
+	var _init bool
+	initDb := &Command{
+		Use:  "db",
+		Long: "init database and create tables that app needs",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("init db...")
+			fmt.Println(args, force, _init)
+		},
+	}
+	initDb.Flags().BoolVarP(&force, "force", "f", false, "force create or delete.")
+	initDb.Flags().BoolVarP(&_init, "init", "i", false, "init a database tables.")
+	rootCmd.AddCommand(initDb)
+}
