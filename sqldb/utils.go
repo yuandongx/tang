68c56090ae01e04dd8717b8433ea2f54d029ae6d
yuandongx <yuandongx@126.com>
2023-05-22 16:03:31 +0800

    U

--- sqldb/utils.go
+++ sqldb/utils.go
@@ -1,5 +1,8 @@
 package sqldb
-
+import (
+	"os",
+	"fmt"
+)
 type _error struct {
 	e string
 }
@@ -9,4 +12,23 @@ func (e _error) Error() string {
 }
 func error_(msg string) *_error {
 	return &_error{e: msg}
+}
+
+
+func GetDbSource()string {
+	host = getEnv("DB_HOST", "127.0.0.1")
+	name = getEnv("DB_NAME", "mysql")
+	username = getEnv("DB_USERNAME", "mysql")
+	password = getEnv("DB_PASSWORD", "P@ssw0rd")
+	port = getEnv("DB_PORT", "3306")
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+	username, password, host, port, name)
+}
+
+func getEnv(key, _default string) string {
+	v, ok := os.LookupEnv(key)
+	if ok {
+		return v
+	}
+	return _default
 }
