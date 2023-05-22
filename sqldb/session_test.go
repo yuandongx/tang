eb8e4c947e47550510d0a5afb421b5c6861a8e1f
yuandongx <yuandongx@126.com>
2023-05-22 18:30:00 +0800

    U

--- sqldb/session_test.go
+++ sqldb/session_test.go
@@ -5,10 +5,15 @@ import (
 )
 
 func TestSession_Query(t *testing.T) {
+	type TangTest struct {
+		A int `type:"INT"`
+		B int `type:"TEXT"`
+	}
 	src := GetPostGresSource()
 	t.Log("database source is : ", src)
 	s := CreatePostgresSession(src)
-	d, e := s.Query("select * from tang_test_123;")
+	s.Create(TangTest{})
+	d, e := s.Query("select * from tang_test;")
 	t.Log(d)
 	t.Log(e)
 }
