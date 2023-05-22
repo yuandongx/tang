b8623ba2a5b3a45ea6ff00b9cfea776eb741503b
yuandongx <yuandongx@126.com>
2023-05-22 16:56:37 +0800

    U

--- sqldb/session_test.go
+++ sqldb/session_test.go
@@ -6,6 +6,7 @@ import (
 
 func TestSession_Query(t *testing.T) {
 	src := GetDbSource()
+	t.Log("database source is : ", src)
 	s := CreateMySqlSession(src)
 	d, e := s.Query("select * from tang_test_123;")
 	t.Log(d)
