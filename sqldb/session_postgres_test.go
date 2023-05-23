2d841e7dde686925ebfca840f92fba17df5e378b
yuandongx <yuandongx@126.com>
2023-05-23 10:10:54 +0800

    update

--- sqldb/session_postgres_test.go
+++ sqldb/session_postgres_test.go
@@ -1,7 +1,6 @@
 package sqldb
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -13,8 +12,7 @@ func TestSession_Query(t *testing.T) {
 	src := GetPostGresSource()
 	t.Log("database source is : ", src)
 	s := CreatePostgresSession(src)
-	i, e := s.Create(TangTest{})
-	fmt.Println(i, e)
+	s.Create(TangTest{})
 	d, e := s.Query("select * from tang_test;")
 	t.Log(d)
 	t.Log(e)
