f04a44402005bc299f0d223b943bd5e4ea36427d
yuandongx <yuandongx@126.com>
2023-05-24 09:23:49 +0800

    U

--- sqldb/factory/postgre_test.go
+++ sqldb/factory/postgre_test.go
@@ -8,7 +8,7 @@ func TestPostgresModel_CreateCaseInt(t *testing.T) {
 	t1 := TangTestCaseInt{}
 	m1 := Parse(t1, POSTGRES, "")
 	got, got1 := m1.Create()
-	want1 := "CREATE TABLE tang_test_case_int(aint integer NOT NULL, bint16 smallint NOT NULL, cint32 integer NOT NULL, dint8 smallint NOT NULL, eint64 bigint NOT NULL, id serial, PRIMARY KEY(id));"
+	want1 := "CREATE TABLE public.tang_test_case_int(aint integer NOT NULL, bint16 smallint NOT NULL, cint32 integer NOT NULL, dint8 smallint NOT NULL, eint64 bigint NOT NULL, id serial, PRIMARY KEY(id));"
 	if want1 == got {
 		t.Log(got, got1)
 	} else {
